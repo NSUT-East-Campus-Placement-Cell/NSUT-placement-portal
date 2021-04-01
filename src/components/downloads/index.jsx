03f71b8ccceb8f3321bb86cfff392e383e443b02
kartik <kartikbhalla12@gmail.com>
2021-04-01 17:24:09 +0530

    downloads: mockup basic downloads page
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/components/downloads/index.jsx
+++ src/components/downloads/index.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import Navbar from '../common/navbar'
+import Footer from '../common/footer'
+import {Table} from 'react-bootstrap'
 
 const Downloads = () => {
     return  (<div className='downloads'>
-        <h1>Downloads</h1>
+        <Navbar />
+        <div className='heading'>
+            <h1>Downloads</h1>
+        </div>
+        <div className="content container">
+            <h2>Check our <span style={{color: '#27ae60' }}>Documents</span></h2>
+            <div className="downloads">
+                <div className="section">
+                    <div className="table-heading">
+                        <h2>Brochure</h2>
+                    </div>
+                    <div className="list">
+                        <a href="/randomguy.jpg" download="brochure">Lorem, ipsum.</a>
+                        <a href="" download="brochure"  >Lorem, ipsum.</a>
+                        <a href="" download="brochure" >Lorem, ipsum.</a>
+                    </div>
+                </div>
+                <div className="section"> 
+                    <div className="table-heading">
+                        <h2>Training Documents</h2>
+                    </div>
+                    <div className="list">
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis voluptates ratione.</a>
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellat, saepe quis molestiae voluptates laudantium sit!</a>
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing</a>
+                    </div>
+                </div>
+                <div className="section">
+                    <div className="table-heading">
+                        <h2>Placement Documents</h2>
+                    </div>
+                    <div className="list">
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure reiciendis voluptates ratione.</a>
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellat, saepe quis molestiae voluptates laudantium sit!</a>
+                        <a href="" download="brochure" >Lorem ipsum dolor sit amet consectetur adipisicing</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer />
     </div>);
 }
  
