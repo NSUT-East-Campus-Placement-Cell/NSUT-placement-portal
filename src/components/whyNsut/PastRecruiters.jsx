c015f39846b06dc19ea8a1ec24d2e4831ca14fe2
Vicky Pandey <vp991058@gmail.com>
2021-04-12 18:21:03 -0700

    aligned the text and fixed the margin

--- src/components/whyNsut/PastRecruiters.jsx
+++ src/components/whyNsut/PastRecruiters.jsx
@@ -11,7 +11,7 @@ export default function PastRecruiters() {
         <h1>PAST RECRUITERS</h1>
       </div>
       <Container>
-      <div style={{marginTop:"10vh",marginBottom:'7vh'}}>
+      <div class='recruiters' style={{marginTop:"10vh",marginBottom:'7vh'}}>
         <center>
           <p>The batch profile consists of students from computer science and electronics background. Students at NSUT East Campus throughout their graduation and Post-Graduation indulge into various skill development programs, internships, training, and cultural as well as technical contests. Students have continued to receive placement offers from various reputed companies from different sectors of the industry.</p>
           <img src="/Companies.jpg" style={{maxWidth:'90%'}} />
