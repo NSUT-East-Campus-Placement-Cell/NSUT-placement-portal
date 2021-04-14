0917d3cb8eb1b0ded965eb2a1e343511a78b695d
kartik <kartikbhalla12@gmail.com>
2021-04-15 02:56:39 +0530

    src: add process section
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/components/process/index.jsx
+++ src/components/process/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Navbar from '../common/navbar';
+import Footer from '../common/footer';
 
 const Process = () => {
 	return (
 		<div className='process'>
-			<h1>Process</h1>
+			<Navbar />
+			<div
+				className='heading'
+				style={{ backgroundImage: `url("/downloads/bg.png")` }}>
+				<h1>Process</h1>
+			</div>
+			<div className='image-wrapper container'>
+				<img
+					src={
+						window.innerWidth > 992
+							? '/process/desktop-process.svg'
+							: '/process/mobile-process.svg'
+					}
+				/>
+			</div>
+			<Footer />
 		</div>
 	);
 };
